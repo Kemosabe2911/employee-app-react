6aed041f3a1626c5c4c8f02b406914cc75c580ef
Cizil <cizilmaria@gmail.com>
2022-07-25 11:49:36 +0530

    Included department list

--- src/components/DepartmentDetailsComponent.tsx
+++ src/components/DepartmentDetailsComponent.tsx
@@ -1,29 +1,35 @@
 import React, { FC } from 'react';
 
-import { DEPARTMENT_DETAIL_ITEM as department } from 'constants/departmentDetailItem';
+import { DEPARTMENT_DETAIL_ITEM as departmentItems } from 'constants/departmentDetailItem';
 
 const DepartmentDetailsComponent: FC = () => {
     return (
-        <table className="table">
+        <table className="mx-auto table-auto">
             <thead>
-                <tr className="ml-32 mt-8  flex w-[100%] rounded-xl bg-white p-5 shadow-xl">
-                    <th className="w-[250px] ">Department Name</th>
-                    <th className="w-[250px]">Department Id</th>
-                    <th className="w-[250px]">Head Count</th>
-                    <th className="w-[250px]">Location</th>
+                <tr className=" mt-8 flex  w-[100%] gap-24  bg-aliceBlue p-5 text-center  shadow-xl ">
+                    <th className="w-[250px] font-normal">Department Name</th>
+                    <th className="w-[250px] font-normal">Department Id</th>
+                    <th className="w-[250px] font-normal">Head Count</th>
+                    <th className="w-[250px] font-normal">Location</th>
                 </tr>
             </thead>
             <tbody>
-                <tr className="ml-32 mt-2 flex  w-[100%]  rounded-xl  bg-white p-5 shadow-xl">
-                    <td className="w-[250px]">{department.departmentName}</td>
-                    <td className="w-[250px]">{department.departmentId}</td>
-                    <td className="w-[250px]">{department.headCount}</td>
-                    <td className="w-[250px]">{department.location}</td>
-                </tr>
+                {departmentItems.map(department => {
+                    return (
+                        <div key={department.departmentId}>
+                            <tr className=" mt-3 flex  w-[100%] gap-24  bg-white p-5  text-center shadow-xl">
+                                <td className="w-[250px]">{department.departmentName}</td>
+                                <td className="w-[250px]">{department.departmentId}</td>
+                                <td className="w-[250px]">{department.headCount}</td>
+                                <td className="w-[250px]">{department.location}</td>
+                            </tr>
+                        </div>
+                    );
+                })}
             </tbody>
         </table>
-      
-        
+
+
     );
 };
 export default DepartmentDetailsComponent;
